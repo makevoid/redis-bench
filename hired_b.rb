6cd805d211c671cb11a84b5840f5994cbe17154b
Francesco Canessa <makevoid@gmail.com>
2013-07-01 13:36:11 +0300

    added another test file

--- hired_b.rb
+++ hired_b.rb
@@ -3,7 +3,7 @@ require "hiredis"
 conn = Hiredis::Connection.new
 conn.connect "127.0.0.1", 6379
 
-TIMES = 10000
+TIMES = 100_000
 
 TIMES.times do
   conn.write ["SET", "foo", "bar#{rand(20)}"]
@@ -11,7 +11,18 @@ TIMES.times do
 end
 
 
-(TIMES*2).times do
+vals = []
+
+(TIMES).times do
+  val = conn.read
   val = conn.read
   # puts val
+  vals << val
 end
+
+tmp = vals.join(",")
+
+File.open("./tmp/tmp.txt", "w") do |file|
+  file.write tmp
+end
+
